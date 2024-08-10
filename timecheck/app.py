ae72f47ce639c5668743877bd4c6c4beb428b154
Lucy Hancock <lucy@leh.dev>
2024-08-10 23:29:40 +0100

    Use NTP for request

--- timecheck/app.py
+++ timecheck/app.py
@@ -1,34 +1,23 @@
-from datetime import datetime, UTC
-from requests import get
+from math import floor
 from tkinter import *
 from tkinter import messagebox
+import ntplib
 
 
 def run():
-    system_time = datetime.now(UTC)
-    req_start = datetime.now()
-    remote_time_resp = get('https://worldtimeapi.org/api/timezone/Etc/UTC')
-    req_end = datetime.now()
-    res = remote_time_resp.json()
-    remote_time = datetime.fromisoformat(res['datetime'])
-    print(f'Time request took {req_end - req_start}')
-    if (req_end - req_start).seconds > 0:
-        print("Time request took too long, results may be inaccurate")
-    time_delta = remote_time - system_time
-    req_time = req_end - req_start
-    if req_time < time_delta:
-        time_delta -= req_time
-    else:
-        print("System time diff lower than request time")
-    print(f"System time differs from remote time by {time_delta}")
+    ntp_client = ntplib.NTPClient()
+    ntp_res = ntp_client.request('uk.pool.ntp.org', version=3)
+
+    time_delta = ntp_res.offset
+    print(f"System time differs from remote time by {time_delta}s")
     root = Tk()
     root.withdraw()
-    if 0 < time_delta.seconds < 30:
-        messagebox.showwarning('TimeCheck', f"System time differs from remote time by {time_delta}")
-    elif time_delta.seconds >= 30:
-        messagebox.showerror('TimeCheck', f"System time differs from remote time by {time_delta}")
+    if 0 < floor(time_delta) < 30:
+        messagebox.showwarning('TimeCheck', f"System time differs from remote time by {round(time_delta, 2)}s")
+    elif floor(time_delta) >= 30:
+        messagebox.showerror('TimeCheck', f"System time differs from remote time by {round(time_delta, 2)}s")
     else:
-        messagebox.showinfo('TimeCheck', f"System time accurate, difference {time_delta}")
+        messagebox.showinfo('TimeCheck', f"System time accurate, difference {round(time_delta, 2)}s")
 
 
 if __name__ == "__main__":
